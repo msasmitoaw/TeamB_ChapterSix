c47179cb18c7ac919f0247a067d202228fd27d2d
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2021-01-11 14:05:01 +0700

    [sash] add ui game play and its assets

---
+++ app/src/main/java/com/rockpaperscissors/team/b/ui/gameplay/GamePlayActivity.kt
@@ -0,0 +1,12 @@
+package com.rockpaperscissors.team.b.ui.gameplay
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rockpaperscissors.team.b.R
+
+class GamePlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_play)
+    }
+}
