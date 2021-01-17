70f63cdf11e88bc05136162fe38775a67c3efe9d
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2021-01-17 19:29:00 +0700

    [sash] refactor context on sharedpref

--- app/src/main/java/com/suit/team/b/data/local/SharedPref.kt
+++ app/src/main/java/com/suit/team/b/data/local/SharedPref.kt
@@ -15,8 +15,7 @@ object SharedPref {
     private const val KEY_SCORE_P2 = "KEY_SCORE_P2"
     private const val KEY_SCORE_CPU = "KEY_SCORE_CPU"
     private const val KEY_USERNAME = "USERNAME"
-    private val con = App.weakReferenceContext.get()
-    private val pref = con?.getSharedPreferences("pref", Context.MODE_PRIVATE)
+    private val pref = App.weakReferenceContext.get()?.getSharedPreferences("pref", Context.MODE_PRIVATE)
 
     fun scoreToPref(playerType: PlayerType, gameType: GameType) {
         when (playerType) {
@@ -57,9 +56,9 @@ object SharedPref {
     }
 
     fun getRankedScoreVsP(): MutableList<Score> {
-        val nameP1 = pref?.getString(KEY_USERNAME, con?.getString(R.string.player1))
+        val nameP1 = pref?.getString(KEY_USERNAME, App.weakReferenceContext.get()?.getString(R.string.player1))
         val scoreValueP1 = pref?.getInt(KEY_SCORE_USER_VSP2, 0)
-        val nameP2 = con?.getString(R.string.player2)
+        val nameP2 = App.weakReferenceContext.get()?.getString(R.string.player2)
         val scoreValueP2 = pref?.getInt(KEY_SCORE_P2, 0)
         val scoreRank = mutableListOf(
                 Score(name = nameP1, scoreValue = scoreValueP1),
@@ -70,9 +69,9 @@ object SharedPref {
     }
 
     fun getRankedScoreVsCPU(): MutableList<Score> {
-        val nameP1 = pref?.getString(KEY_USERNAME, con?.getString(R.string.player1))
+        val nameP1 = pref?.getString(KEY_USERNAME, App.weakReferenceContext.get()?.getString(R.string.player1))
         val scoreValueP1 = pref?.getInt(KEY_SCORE_USER_VSCPU, 0)
-        val nameCPU = con?.getString(R.string.CPU)
+        val nameCPU = App.weakReferenceContext.get()?.getString(R.string.CPU)
         val scoreValueCPU = pref?.getInt(KEY_SCORE_CPU, 0)
         val scoreRank = mutableListOf(
                 Score(name = nameP1, scoreValue = scoreValueP1),
@@ -80,24 +79,5 @@ object SharedPref {
         )
         scoreRank.sortByDescending { it.scoreValue }
         return scoreRank
-    }
-
-    //FOR DUMMY
-    fun getScore(playerType: PlayerType, gameType: GameType? = null): String {
-        return when (playerType) {
-            PlayerType.P1 -> {
-                if (gameType == GameType.VSP) {
-                    pref?.getInt(KEY_SCORE_USER_VSP2, 0).toString()
-                } else {
-                    pref?.getInt(KEY_SCORE_USER_VSCPU, 0).toString()
-                }
-            }
-            PlayerType.P2 -> {
-                pref?.getInt(KEY_SCORE_P2, 0).toString()
-            }
-            PlayerType.CPU -> {
-                pref?.getInt(KEY_SCORE_CPU, 0).toString()
-            }
-        }
     }
 }
