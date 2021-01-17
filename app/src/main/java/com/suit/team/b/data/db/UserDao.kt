b15649ccc6cad88d2801a824ae4db1e5e4d904cc
maulana ilham <m.ilham04@gmail.com>
2021-01-17 16:54:23 +0700

    [ilham] modifying UserDao

--- app/src/main/java/com/suit/team/b/data/db/UserDao.kt
+++ app/src/main/java/com/suit/team/b/data/db/UserDao.kt
@@ -1,14 +1,14 @@
 package com.suit.team.b.data.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.suit.team.b.data.model.User
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertUser(userEntity: UserEntity)
     @Query("SELECT * FROM Users WHERE id = :id LIMIT 1")
-    fun fetchUserbyId(id: Int): UserEntity
+    fun fetchUserById(id: Int): UserEntity
+    @Query("DELETE FROM Users WHERE id = :id")
+    fun deleteUserById(id:Int): Int
 }
