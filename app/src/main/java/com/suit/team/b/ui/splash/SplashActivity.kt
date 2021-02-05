ec11ccac3e926c20f1efeeedf6c9b1e01010863d
sofimuhammad <sofimuhammad877@gmail.com>
2021-02-05 22:28:45 +0700

    update splash screen

--- app/src/main/java/com/suit/team/b/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/suit/team/b/ui/splash/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.suit.team.b.ui.splash
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.suit.team.b.R
 import com.suit.team.b.ui.main.MainActivity
 import com.suit.team.b.ui.slide.SlideActivity
@@ -14,10 +16,33 @@ class SplashActivity : AppCompatActivity(), SplashView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler(Looper.getMainLooper()).postDelayed({
+        /*Handler(Looper.getMainLooper()).postDelayed({
             presenter = SplashPresenterImp(this)
             presenter?.checkIsLogin()
-        }, 1500)
+        }, 1500)*/
+        val lottie = findViewById<LottieAnimationView>(R.id.lottie_splash)
+
+        lottie.apply {
+            addAnimatorListener(object: Animator.AnimatorListener{
+                override fun onAnimationRepeat(animation: Animator?) {
+
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    presenter = SplashPresenterImp(this@SplashActivity)
+                    presenter?.checkIsLogin()
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+
+                }
+
+                override fun onAnimationStart(animation: Animator?) {
+
+                }
+
+            })
+        }
     }
 
     override fun onLogged() {
