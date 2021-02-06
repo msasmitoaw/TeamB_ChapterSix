cb390d8ce08696c48c30ffcf5188cd0c121cccf1
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2021-02-06 20:55:42 +0700

    [sash] add animation on register loading button

--- app/src/main/java/com/suit/team/b/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/suit/team/b/ui/register/RegisterActivity.kt
@@ -3,84 +3,84 @@ package com.suit.team.b.ui.register
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
-import android.widget.Button
-import android.widget.EditText
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.textfield.TextInputLayout
 import com.suit.team.b.R
 import com.suit.team.b.data.model.RegisterRequest
 import com.suit.team.b.data.remote.ApiModule
+import com.suit.team.b.databinding.ActivityRegisterBinding
 import com.suit.team.b.ui.auth.AuthActivity
 import com.suit.team.b.utils.string
-import com.suit.team.b.utils.text
 
 class RegisterActivity : AppCompatActivity() {
-    private val etUsername: EditText by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.etUsername) }
-    private val etPassword: EditText by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.etPassword) }
-    private val etEmail: EditText by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.etEmail) }
-    private val tilUsername: TextInputLayout by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.tilUsername) }
-    private val tilPassword: TextInputLayout by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.tilPassword) }
-    private val tilEmail: TextInputLayout by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.tilEmail) }
-    private val btnRegister: Button by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.btnRegister) }
+    private lateinit var bind: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        bind = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(bind.root)
 
         val factory = RegisterViewModel.Factory(ApiModule.service)
         val viewModel = ViewModelProvider(this, factory)[RegisterViewModel::class.java]
 
-        btnRegister.setOnClickListener {
-            tilEmail.error = null
-            tilPassword.error = null
-            tilUsername.error = null
+        bind.btnRegister.setOnClickListener {
+            bind.tilEmail.error = null
+            bind.tilPassword.error = null
+            bind.tilUsername.error = null
 
-            if (etUsername.text.isBlank()) {
-                tilUsername.error = string(R.string.username_validation)
+            if (bind.etUsername.text!!.isBlank()) {
+                bind.tilUsername.error = string(R.string.username_validation)
                 return@setOnClickListener
             }
-            if (etPassword.text.isBlank()) {
-                tilPassword.error = string(R.string.password_validation)
+            if (bind.etPassword.text!!.isBlank()) {
+                bind.tilPassword.error = string(R.string.password_validation)
                 return@setOnClickListener
             }
-            if (etEmail.text.isBlank()) {
-                tilEmail.error = string(R.string.email_validation)
+            if (bind.etEmail.text!!.isBlank()) {
+                bind.tilEmail.error = string(R.string.email_validation)
                 return@setOnClickListener
             }
-            if (etUsername.text.length < 6) {
-                tilUsername.error = string(R.string.username_shorter)
+            if (bind.etUsername.text!!.length < 6) {
+                bind.tilUsername.error = string(R.string.username_shorter)
                 return@setOnClickListener
             }
-            if (!Patterns.EMAIL_ADDRESS.matcher(etEmail.text).matches()) {
-                tilEmail.error =
+            if (!Patterns.EMAIL_ADDRESS.matcher(bind.etEmail.text!!).matches()) {
+                bind.tilEmail.error =
                     string(R.string.email_format_validation)
                 return@setOnClickListener
             }
 
             val registerRequest = RegisterRequest(
-                username = etUsername.text(),
-                password = etPassword.text(),
-                email = etEmail.text()
+                username = bind.etUsername.text.toString(),
+                password = bind.etPassword.text.toString(),
+                email = bind.etEmail.text.toString()
             )
+
+            it.isEnabled = false
+            bind.lottieRegisterLoading.visibility = View.VISIBLE
+            bind.lottieRegisterLoading.playAnimation()
             viewModel.register(registerRequest)
         }
 
         viewModel.onErrorRegister().observe(this, {
             when (it) {
-                R.string.username_exist.toString() -> tilUsername.error =
+                R.string.username_exist.toString() -> bind.tilUsername.error =
                     string(R.string.username_exist)
-                R.string.email_exist.toString() -> tilEmail.error = string(R.string.email_exist)
-                R.string.username_not_alphanumeric.toString() -> tilUsername.error =
+                R.string.email_exist.toString() -> bind.tilEmail.error =
+                    string(R.string.email_exist)
+                R.string.username_not_alphanumeric.toString() -> bind.tilUsername.error =
                     string(R.string.username_not_alphanumeric)
-                R.string.username_shorter.toString() -> tilUsername.error =
+                R.string.username_shorter.toString() -> bind.tilUsername.error =
                     string(R.string.username_shorter)
                 else -> Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
             }
+            onAnimationFinish()
         })
 
         viewModel.onResultRegister().observe(this, {
+            onAnimationFinish()
             Toast.makeText(this, string(R.string.sign_up_success), Toast.LENGTH_SHORT).show()
             startActivity(Intent(this, AuthActivity::class.java))
             finish()
@@ -90,5 +90,11 @@ class RegisterActivity : AppCompatActivity() {
     override fun onBackPressed() {
         startActivity(Intent(this, AuthActivity::class.java))
         finish()
+    }
+
+    private fun onAnimationFinish() {
+        bind.lottieRegisterLoading.pauseAnimation()
+        bind.lottieRegisterLoading.visibility = View.INVISIBLE
+        bind.btnRegister.isEnabled = true
     }
 }
