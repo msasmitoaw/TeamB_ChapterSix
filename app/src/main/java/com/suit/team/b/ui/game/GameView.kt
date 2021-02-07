d17d915b124a914677da081290dc9acc4e65d760
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2021-02-07 21:59:07 +0700

    [sash] remove mvp file pattern of gameplay and add viewmodel for gameplay

--- app/src/main/java/com/suit/team/b/ui/game/GameView.kt
+++
@@ -1,9 +0,0 @@
-package com.suit.team.b.ui.game
-
-interface GameView {
-    fun onPlayerOnePick()
-    fun onPlayerTwoPick()
-    fun onComputerPick()
-    fun onResult(result: String)
-    fun onRefresh()
-}
