06c0ca06304430206a9c1bb602b972df485e97f5
maulana ilham <m.ilham04@gmail.com>
2021-02-10 13:52:50 +0700

    [Ilham][Bugfix][feature/profile] toast message showing when initiaing page, update shared preference when API updated.

--- app/src/main/java/com/suit/team/b/ui/score/ScoreActivity.kt
+++ app/src/main/java/com/suit/team/b/ui/score/ScoreActivity.kt
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.MODE_FIXED
-import com.google.android.material.tabs.TabLayout.MODE_SCROLLABLE
 import com.google.android.material.tabs.TabLayoutMediator
 import com.suit.team.b.R
 import com.suit.team.b.ui.main.MainActivity
